9384cd76e2e0d127d779d5585dd558a726be2b59
Ubuntu <ubuntu@ip-172-31-44-33.eu-central-1.compute.internal>
2026-03-14 09:59:22 +0000

    Fix admin.py registration

---
+++ bets/migrations/0001_initial.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+    initial = True
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name='Snail',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+                ('photo', models.ImageField(upload_to='snails/')),
+            ],
+        ),
+    ]
